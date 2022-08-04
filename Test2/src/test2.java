8c44e88ef18b585a95ca2013d727ebd14e505ee5
Allenwangxy <109071846+Allenwangxy@users.noreply.github.com>
2022-08-04 17:47:18 +0800

    Add files via upload

---
+++ Test2/src/test2.java
@@ -0,0 +1,13 @@
+public class test2 {
+    public static void main(String[] args) {
+        int a = (int)(Math.random()*100);
+        System.out.println(a);
+        if(a>50){
+            System.out.println("large");
+        }
+        else{
+            System.out.println("small");
+        }
+
+    }
+}
