5bba7e208fab2b31f4ca6f97f31abb2343508cbc
Allenwangxy <109071846+Allenwangxy@users.noreply.github.com>
2022-08-09 18:57:57 +0800

    Add files via upload

---
+++ Test5/src/test5.java
@@ -0,0 +1,11 @@
+public class test5 {
+    public static void main(String[] args) {
+        int a=0;
+        int num=0;
+        while (a<=100){
+            num=num+a;
+            a++;
+        }
+        System.out.println(num);
+    }
+}
